efa5c91e1bfaecf3bb4bb5af0afaa3d7e6c2bf7e
Jerez <danieljerez@smandes.edu.ar>
2024-02-19 16:16:28 -0300

    Login y registro funcionando

---
+++ HILO/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCKVA_0z6i9HMOhHdQF3pzn5FIXgX_-_-Y",
+    authDomain: "hilo-app-d6d60.firebaseapp.com",
+    projectId: "hilo-app-d6d60",
+    storageBucket: "hilo-app-d6d60.appspot.com",
+    messagingSenderId: "506976867666",
+    appId: "1:506976867666:web:63d392630472e916c5168b"
+  };
+
+const app = initializeApp(firebaseConfig) 
+export default app;
